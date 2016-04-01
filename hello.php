26e8990806c0898aa4beef2e7f469bea108b46ee
VellaNicholas <100608129@student.swin.edu.au>
2016-04-01 15:48:36 +1100

    Update hello.php

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  	</head>
  	<body>
 		<?php
-			// Connects to the XE service (i.e. database) on the "localhost" machine
+			//establishes connection to the database, and outputs either 'Connected' or an Oracle exception
 			$conn = oci_connect('s100608129', 'jxAsPZN2', 'db.nicholaslvella.com/DBITCS');
 			if (!$conn) {
 			    $e = oci_error();
@@ -13,9 +13,11 @@
 				echo "Connected to Database";
 			}
 		
+			//prepares and executes the SQL Statement
 			$stid = oci_parse($conn, 'SELECT * FROM Employee');
 			oci_execute($stid);
 			
+			//creates a table, populating it with data from the SQL statement
 			echo "<table border='1'>\n";
 			while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 			    echo "<tr>\n";
